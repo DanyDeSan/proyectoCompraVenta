d058ea8d0b2f9027dcbec4424f0e00ab960fddfb
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2016-04-04 20:53:14 -0500

    Agregué mas funciones que van siguiendo la tarea principal del programa

--- compraVenta.c
+++ compraVenta.c
@@ -1,14 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+typedef struct producto
+{
+	int valor;
+	unsigned char nombreProducto[30];
+	unsigned char precioProdu[30];
+	unsigned char hayProdu[30];
+	float precio;
+	unsigned int hay;
+	struct producto *sig;
+
+}Producto;
 
 void imprimirMenuPrincipal();
-void desplegarListaProductos(FILE *Prodcutos);
+void desplegarListaProductos(FILE *registro);
+int contarTipoProducto(FILE *registro);
+Producto* crearListaProductos(FILE *registro, int numeroProductos);
+Producto* asignarInformacionProductos(Producto* lista, FILE* registro, int numeroProductos);
 
 int main(int argc, char const *argv[])
 {
-	//Apuntadores para abrir los archivos.
-	FILE *Prodcutos;
-	FILE *Cuentas;
+	FILE *registro; 
 	//Variable que almacena la opcion introducida por el teclado
 	int Opcion;
 
@@ -22,40 +36,82 @@ int main(int argc, char const *argv[])
 		//Seleccion de departamento 
 		switch(Opcion)
 		{
-			//En cada caso, excepto el default,se va a abrir primero el archivo,
-			//se comprobará de que el archivo existe, de lo contrario se lanzará
-			//mensaje de error. Luego se desplegará la lista de prodcutos disponibles,
-			//con su respectivo precio, para ello los precios estarán almacenados en un
-			//arreglo, los productos cuyas existencias sean cero no aparecerán en la lista.
+			//Este switch es básicamente para que el flujo del programa siga mientras 
+			//se escoja una opción diferente a la 6.
 			case 1:
-
+				registro = fopen("electronica.txt","r");
+				if( registro == NULL)
+				{
+					printf("Lo sentimos el sistema no está disponible\n Contacte al administrador\n");
+					continue;
+				}else{
+					desplegarListaProductos(registro);
+				}
 			break;
 
 			case 2:
+				registro = fopen("papeleria.txt","r");
+				if( registro == NULL)
+				{
+					printf("Lo sentimos el sistema no está disponible\n Contacte al administrador\n");
+					continue;
+				}else{
+					desplegarListaProductos(registro);
+				}
 
 			break; 
 
 			case 3:
+				registro = fopen("ropa.txt","r");
+
+				if( registro == NULL)
+				{
+					printf("Lo sentimos el sistema no está disponible\n Contacte al administrador\n");
+					continue;
+				}else{
+					desplegarListaProductos(registro);
+				}
 
 			break;
 
 			case 4:
+				registro = fopen("muebles.txt","r");
+				if( registro == NULL)
+				{
+					printf("Lo sentimos el sistema no está disponible\n Contacte al administrador\n");
+					continue;
+				}else{
+					desplegarListaProductos(registro);
+				}
 
 			break; 
 
 			case 5:
+				registro = fopen("deportes.txt","r");
+				if( registro == NULL)
+				{
+					printf("Lo sentimos el sistema no está disponible\n Contacte al administrador\n");
+					continue;
+				}else{
+					desplegarListaProductos(registro);
+				}
 
 			break; 
 
 			case 6:
 
+			printf("Gracias por compra con nostros!!\nVuelva Pronto!!\n\n");
+
 			break;
 
 			default:
 
+			printf("Por favor escoja una opción correcta\n");
+			
 			break;
 		}
-	}
+	fclose(registro);
+	}while(Opcion != 6);
 	return 0;
 }
 
@@ -72,24 +128,100 @@ Description:
 void imprimirMenuPrincipal()
 {
 	printf("Departamentos:\n");
-	printf("1. Departamento de electronica\n2. Departamento de Comida\n3. Departamento de Ropa\n4. Departamento Muebles\n");
+	printf("1. Departamento de electronica\n2. Departamento de Papelería\n3. Departamento de Ropa\n4. Departamento Muebles\n");
 	printf("5. Departamento de Deportes\n6. Salir del Sistema\n\n");
 	printf("Opción: ");
 }
 
-
 ////////////////////////////////////
 //Function: desplegarListaProductos
-//in: apuntador de tipo FILE
-//out: void 
+//in: Apuntador de tipo FILE que apunta al archivo registros
+//out: void
 /*
-Description:
-	La función imprime en forma de lista los productos guardados en un archivo,
-	con sus respectivos precios, los productos con existencias igual a 0
-	no se desplegarán en la lista. 
-
+Description: 
+	
+	En esta funcion se realizan las siguientes operaciones:
+		- contar tipo de productos
+		- crear arreglo dinamico de productos
+		- asignación de informacion de los productos al arreglo
+		- Se despliega la lista de productos
+		- Se realiza la operacion de compra
+		- Se actualiza el archivo registro de productos en caso 
+			de que la existencia de un producto sea igual a cero.
 */
-void desplegarListaProductos(FILE *Prodcutos)
+
+void desplegarListaProductos(FILE *registro)
 {
+	//Primero contamos el números de productos, por tipo de producto, no por existencias.
+	int numeroProductos = contarTipoProducto(registro);
+	Producto *lista = crearListaProductos(registro,numeroProductos);
+	lista = asignarInformacionProductos(lista,registro,numeroProductos);
 
 }
+////////////////////////////////////
+//Function: contarTipoProducto
+//in: Apuntador de tipo FILE que apunta al archivo de registros
+//out: entero
+/*
+Description: 
+	La funcion se encarga de contar el número de líneas que
+	hay en el archivo, lo que equivale el número de productos
+	que hay registrados.
+*/
+
+int contarTipoProducto(FILE *registro)
+{
+	int contador = 0;
+	char caracter;
+
+	while(feof(registro) == 0)
+	{
+		caracter = fgetc(registro);
+		if(caracter == '\n')
+		{
+			contador ++; 
+		}
+	}
+	rewind(registro);
+	return contador;
+}
+
+////////////////////////////////////
+//Function: crearListaProdcutos
+//in: Apuntador de tipo FILE que apunta al archivo de registros
+//out: Apuntador de tipo Producto
+/*
+Description: 
+	
+	La función se encarga de crear una arreglo en memoria dinámica
+	con esapacios equivalentes a la estructura de dato Producto. 
+*/
+
+Producto *crearListaProductos(FILE *registr,int numeroProductos)
+{
+	Producto *lista = NULL;
+	lista = (Producto *) calloc(numeroProductos, sizeof(Producto));
+	return lista;
+}
+
+////////////////////////////////////
+//Function: asignarInformacionProductos
+//in: apuntador de tipo producto, apuntador de tipo FILE que apunta al archivo registro, entero numero productos
+//out: apuntador de tipo productos
+/*
+Description: 
+	
+	En esta función se asigna la informacion proveniente del archivo registro al arreglo de productos	
+
+*/
+
+Producto *asignarInformacionProductos(Producto* lista, FILE *registro, int numeroProductos)
+{
+	for(int i = 0; i < numeroProductos; i++)
+	{
+		fscanf(registro,"%s\t%s\t%s",lista[i].nombreProducto,lista[i].precioProdu,lista[i].hayProdu);
+	}
+	return lista;
+}
+
+
